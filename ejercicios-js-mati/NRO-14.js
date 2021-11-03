cc8ae5ccc8288ac04752757c62bd1f493af78f5c
ianpatrani <ianpatrani@hotmail.com>
2021-11-03 13:24:25 -0300

    Ready

--- ejercicios-js-mati/NRO-14.js
+++ ejercicios-js-mati/NRO-14.js
@@ -3,9 +3,9 @@ buscar en este array ["lorem", "ipsum", "scammer", "simply", "laboris", "sit", "
 cuales elementos tienen el valor de "laboris" y convertirlos a mayuscula
 output => ["lorem", "ipsum", "scammer", "simply", "LABORIS", "sit", "amet", "LABORIS"] */
 let array = ["lorem", "ipsum", "lorem", "scammer", "simply", "laboris", "sit", "amet", "laboris"];
-array.forEach((element, index) => {
+array.forEach((element, i) => {
     if (element == "laboris") {
-        array[index] = array[index].toUpperCase()
+        array[i] = array[i].toUpperCase()
     }
 });
 console.log(array)
--- ejercicios-js-mati/NRO-15.js
+++ ejercicios-js-mati/NRO-15.js
@@ -1,3 +1,6 @@
 /* NRO-15
 determinar si el valor de foo="qux" es de valor par o impar
 ouput => mostrar resultado */
+let foo = "qux";
+
+console.log();
